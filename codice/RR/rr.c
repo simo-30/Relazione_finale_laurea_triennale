6cb8eca3003f01b50396f56afcf1a372c10f9066
Simone <trenta.1724141@studenti.uniroma1.it>
2020-11-17 11:14:28 +0100

    ricomincio RR

--- codice/RR/rr.c
+++ codice/RR/rr.c
@@ -25,37 +25,10 @@ void rr_scheduler(SettingType* setting, const char* result) {
 	CoreType* core_list=new_CoreType_list(setting->core, QUANTUM); 
 	for (timing=0; timing<=setting->max_time; timing++) {
 		/**Implementazione vera e propria della politica**/
-		rr_to_ready_proc(list, timing, pid_proc, setting->pid);
-		if (count_is_running(list) < setting->core) {
-			//processo in running
-			rr_to_running_proc(list, timing, pid_proc, setting->pid, core_list, setting->core);
-		}
-		else {
-			//processo in waiting
-			rr_to_waiting_proc(list, timing, pid_proc, setting->core);
-		}
-		rr_run_proc(list, core_list, setting->core, pid_proc, setting->pid);
-		rr_to_burst_proc(list);
-		rr_new_burst_proc(list, timing, setting->max_time, setting->avg_time);
-		update_statistics(list, stat);
-		print_listProcess(list);
 	}
-	while (count_state_to_terminated(list) != 0) {
-		rr_to_ready_proc(list, timing, pid_proc, setting->pid);
-		if (count_is_running(list) < setting->core) {
-			//processo in running
-			rr_to_running_proc(list, timing, pid_proc, setting->pid, core_list, setting->core);
-		}
-		else {
-			//processo in waiting
-			rr_to_waiting_proc(list, timing, pid_proc, setting->core);
-		}
-		rr_run_proc(list, core_list, setting->core, pid_proc, setting->pid);
-		rr_to_terminated_proc(list);
-		print_listProcess(list);
-		update_statistics(list, stat);
-	}
-	write_on_file(stat, result);
+	//while (count_state_to_terminated(list) != 0) {
+	//}
+	//write_on_file(stat, result);
 	return;
 }
 
@@ -85,27 +58,6 @@ void print_CoreType_list(CoreType* core, int dim) {
 	return;
 }
 
-void rr_to_ready_proc(ListProcess* list, int timing, int* proc, int dim) {
-	int i;
-	ProcessItem* aux=(ProcessItem*)malloc(sizeof(ProcessItem));
-	aux=list->first;
-	for (i=0; i<list->size; i++) {
-		if (aux->info->time_arrive == timing) {
-			if (aux->info->state == NOT_STATE) {
-				//processo nuovo appena arrivato
-				to_ready(aux->info);
-				insert_first_possible(proc, dim, aux->info->pid);
-			}
-			if (aux->info->state == BURST) {
-				to_ready(aux->info);
-				insert_first_possible(proc, dim, aux->info->pid);
-			}
-		}
-		aux=aux->next;
-	}
-	return;
-}
-
 void insert_into_cores(CoreType* core, int pid, int dim) {
 	int i;
 	for (i=0; i<dim; i++) {
@@ -116,79 +68,6 @@ void insert_into_cores(CoreType* core, int pid, int dim) {
 		}
 	}
 	return;
-}
-
-void rr_to_running_proc(ListProcess* list, int timing, int* proc, int dim_proc, CoreType* core, int dim_core) {
-	int i;
-	for (i=0; i<dim_proc; i++) {
-		if (proc[i] == -1) {
-			return;
-		}
-		ProcessItem* aux=(ProcessItem*)malloc(sizeof(ProcessItem));
-		aux=list->first;
-		int j;
-		for (j=0; j<list->size; j++) {
-			if (proc[i] == aux->info->pid) {
-				if (is_waiting(aux->info) == 1) {
-					to_run(aux->info);
-					scaling_array(proc, dim_proc, aux->info->pid);
-					insert_into_cores(core, aux->info->pid, dim_core);
-					if (count_is_running(list) == dim_core) {
-						rr_to_waiting_proc(list, timing, proc, dim_proc);
-						return;
-					}
-				}
-				if (is_ready(aux->info) == 1) {
-					to_run(aux->info);
-					scaling_array(proc, dim_proc, aux->info->pid);
-					insert_into_cores(core, aux->info->pid, dim_core);
-					if (count_is_running(list) == dim_core) {
-						rr_to_waiting_proc(list, timing, proc, dim_proc);
-						return;
-					}
-				}
-			}
-			aux=aux->next;
-		}
-	}
-	return;
-}
-
-void rr_to_waiting_proc(ListProcess* list, int timing, int* proc, int dim) {
-	int i;
-	ProcessItem* aux=(ProcessItem*)malloc(sizeof(ProcessItem));
-	aux=list->first;
-	for (i=0; i<list->size; i++) {
-		if (is_ready(aux->info) == 1) {
-			to_wait(aux->info);
-		}
-		aux=aux->next;
-	}
-	return;
-}
-
-void rr_run_proc(ListProcess* list, CoreType* core, int dim_core, int* proc, int dim_proc) {
-	int i;
-	ProcessItem* aux=(ProcessItem*)malloc(sizeof(ProcessItem));
-	aux=list->first;
-	for (i=0; i<list->size; i++) {
-		if (is_running(aux->info) == 1) {
-			aux->info->duration-=1;
-			int j;
-			for (j=0; j<dim_core; j++) {
-				if (core[j].pid == aux->info->pid) {
-					core[j].core_time-=1;
-					if (core[j].core_time == 0) {
-						scaling_core_list(core, dim_core, aux->info->pid);
-						to_wait(aux->info);
-						insert_first_possible(proc, dim_proc, aux->info->pid);
-					}
-				}
-			}
-		}
-		aux=aux->next;
-	}
-	return;			
 }
 
 void scaling_core_list(CoreType* core, int dim, int val) {
@@ -210,34 +89,6 @@ void scaling_core_list(CoreType* core, int dim, int val) {
 	return;
 }
 
-void rr_to_burst_proc(ListProcess* list) {
-	int i;
-	ProcessItem* aux=(ProcessItem*)malloc(sizeof(ProcessItem));
-	aux=list->first;
-	for (i=0; i<list->size; i++) {
-		//if (is_running(aux->info) == 1) {
-			if (aux->info->duration == 0) {
-				to_burst(aux->info);
-			}
-		//}
-		aux=aux->next;
-	}
-	return;
-}
-
-void rr_new_burst_proc(ListProcess* list, int min_time, int max_time, int avg_time) {
-	int i;
-	ProcessItem* aux=(ProcessItem*)malloc(sizeof(ProcessItem));
-	aux=list->first;
-	for (i=0; i<list->size; i++) {
-		if (is_burst(aux->info) == 1) {
-			new_burst_for_process(aux->info, min_time, max_time, avg_time);
-		}
-		aux=aux->next;
-	}
-	return;
-}
-
 int count_state_to_terminated(ListProcess* list) {
 	int state;
 	state=count_is_not_state(list) + count_is_ready(list) + count_is_running(list) + count_is_waiting(list) + count_is_burst(list);
@@ -245,19 +96,4 @@ int count_state_to_terminated(ListProcess* list) {
 		return 1;
 	}
 	return 0;
-}
-
-void rr_to_terminated_proc(ListProcess* list) {
-	int i;
-	ProcessItem* aux=(ProcessItem*)malloc(sizeof(ProcessItem));
-	aux=list->first;
-	for (i=0; i<list->size; i++) {
-		if (is_running(aux->info) == 1) {
-			if (aux->info->duration == 0) {
-				to_terminated(aux->info);
-			}
-		}
-		aux=aux->next;
-	}
-	return;
 }
